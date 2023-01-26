e98b0cb34fba81742e6235968dc4e04a01626d87
Franke266 <ifranjic099@gmail.com>
2023-01-26 22:19:43 +0100

    new routes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Vehicles from "./Pages/Vehicles";
 import EditVehicle from "./Components/Vehicles/Edit/EditVehicle";
+import CreateVehicle from "./Components/Vehicles/Create/CreateVehicle";
+import DeleteVehicle from "./Components/Vehicles/Delete/DeleteVehicle";
 import "./App.css";
 
 function App() {
@@ -10,6 +12,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Vehicles />}></Route>
           <Route path="/vehicles/edit/id/:id" element={<EditVehicle />}></Route>
+          <Route path="/vehicles/create" element={<CreateVehicle />}></Route>
+          <Route
+            path="/vehicles/delete/id/:id"
+            element={<DeleteVehicle />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
